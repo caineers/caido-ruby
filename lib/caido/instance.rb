d8e315e66c9b8b198c6ed99feb10bfc93d39f7f4
HAHWUL <hahwul@gmail.com>
2024-06-23 23:28:29 +0900

    chore: Refactor Caido::Instance class initialization and add auth_from_env method

--- lib/caido/instance.rb
+++ lib/caido/instance.rb
@@ -11,13 +11,33 @@ module Caido
   class Instance
     attr_reader :graphql_url, :authorization
 
-    def initialize(graphql_url, authorization)
-      @graphql_url = graphql_url
-      @authorization = if 'Bearer '.include?(authorization)
-                         authorization
-                       else
-                         "Bearer #{authorization}"
-                       end
+    def initialize(*args)
+      @graphql_url = 'http://localhost:8080/graphql'
+      @authorization = nil
+
+      case args.size
+      when 0
+        # Initialize with default values
+      when 1
+        # Initialize with graphql_url
+        @graphql_url = args[0]
+      when 2
+        # Initialize with graphql_url and authorization
+        @graphql_url, authorization = args
+        @authorization = if authorization.include?('Bearer ')
+                           authorization
+                         else
+                           "Bearer #{authorization}"
+                         end
+      else
+        raise ArgumentError, 'Too many arguments provided'
+      end
+
+      auth_from_env if @authorization.nil?
+    end
+
+    def auth_from_env
+      @auth_from_env ||= ENV.fetch('CAIDO_AUTH_TOKEN', 'Bearer ')
     end
 
     def query(query)
