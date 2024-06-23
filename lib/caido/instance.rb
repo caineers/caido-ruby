e8e773a5f509f839bab8a465cc4efd5c305aa3a8
HAHWUL <hahwul@gmail.com>
2024-06-23 23:36:23 +0900

    chore: Refactor Caido::Instance class initialization

--- lib/caido/instance.rb
+++ lib/caido/instance.rb
@@ -4,7 +4,7 @@ require 'httparty'
 require 'json'
 
 # Assuming this code is at the top level of instance.rb and helplers directory is at the same level
-Dir[File.expand_path('helpers/*.rb', __dir__)].sort.each { |file| require file }
+Dir[File.expand_path('helpers/*.rb', __dir__)].each { |file| require file }
 
 module Caido
   # Instance class
@@ -12,46 +12,36 @@ module Caido
     attr_reader :graphql_url, :authorization
 
     def initialize(*args)
+      set_defaults
+      process_arguments(args)
+      auth_from_env
+    end
+
+    private
+
+    def set_defaults
       @graphql_url = 'http://localhost:8080/graphql'
       @authorization = nil
+    end
 
+    def process_arguments(args)
       case args.size
-      when 0
-        # Initialize with default values
       when 1
-        # Initialize with graphql_url
         @graphql_url = args[0]
       when 2
-        # Initialize with graphql_url and authorization
         @graphql_url, authorization = args
-        @authorization = if authorization.include?('Bearer ')
-                           authorization
-                         else
-                           "Bearer #{authorization}"
-                         end
-      else
+        @authorization = format_authorization(authorization)
+      when args.size > 2
         raise ArgumentError, 'Too many arguments provided'
       end
+    end
 
-      auth_from_env if @authorization.nil?
+    def format_authorization(auth)
+      auth.include?('Bearer ') ? auth : "Bearer #{auth}"
     end
 
     def auth_from_env
       @auth_from_env ||= ENV.fetch('CAIDO_AUTH_TOKEN', 'Bearer ')
-    end
-
-    def query(query)
-      res = HTTParty.post(
-        graphql_url,
-        body: { query: query }.to_json,
-        headers: {
-          'Content-Type' => 'application/json',
-          'Authorization' => authorization
-        }
-      )
-
-      obj = JSON.parse(res.body)
-      obj['data']
     end
   end
 end
